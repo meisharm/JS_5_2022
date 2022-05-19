56bcb2307e98f80b12b07db9306983a9471d70b0
Meishar Meiri <meishar@PSIL-MBP-036.fritz.box>
2022-05-19 20:44:10 +0200

    first commit

---
+++ js_lesson_code_1.js
@@ -0,0 +1,124 @@
+/* variables type 
+number
+string
+boolean 
+null
+undefined 
+*/
+
+/* declaration 
+let - if we need too change later
+const - if we don't want to change later
+var - old, scope problem
+no declaration - scope problem
+*/
+
+let age =17
+let userName =  "shimon" //backtick 
+
+
+
+
+// 2+2  "str1"+"str2"  name[0] / % *  age++ age+1
+userName.toLowerCase()
+userName.trim()
+userName.indexOf("o")
+userName.slice(3,7)
+userName.replace("on","shon")
+console.log("hello " + userName)
+// 
+
+let isOnLine = false
+
+console.log(3<2)
+
+console.log(1<2&&2<3)
+
+
+// 
+
+
+if(age>18){
+    console.log("access allowed")
+}else{
+    console.log("access denied")
+}
+
+
+
+// 
+
+
+for (let i = 0; i <10; i++) {   
+    console.log(i)
+}
+
+
+
+
+// 
+
+
+const add2 = function(number){
+    return number+2
+}
+
+5<2 && 8>2
+
+!3<2
+
+!true
+
+!false
+
+25%7
+
+let counter = 0
+
+counter = counter +1 
+
+counter += 4
+
+counter++
+
+counter
+
+++counter
+
+counter
+
+counter+=13
+
+"sfhgbflsjh"+="nsknsdv"
+
+let myStr ="sfhgbflsjh"; mystr+="nsknsdv";
+
+
+"1"+=1
+
+
+13<21
+
+
+let myBool = 13<21; myBool
+
+typeof myBool
+
+
+let myBool1 = false;let myBool2 = true;myBool1&&myBool2
+
+myBool1||myBool2
+
+3<2 || 2>1
+
+3<2 && 2>1
+
+!3<2
+
+!true
+
+!false
+
+userEmail = null
+typeof userEmail
+'object'

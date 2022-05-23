68b6a75984e10449ee276b665424b828cf67bae1
Meishar Meiri <Meishar@gmail.com>
2022-05-23 21:41:44 +0200

    23_05_3

--- first_js/index.js
+++
@@ -1,5 +0,0 @@
-const myName = "Meishar"
-const bodyObj = document.getElementsByTagName("body")
-const helloObj = document.createElement("h1")
-helloObj.innerHTML =`Hi ${myName}, Hello from JS `
-document.body.appendChild(helloObj)

8d1abacf6e8b7349e9f0c704d1e3eb1843c15974
Meishar Meiri <Meishar@gmail.com>
2022-06-28 15:48:19 +0200

    28.06.350

---
+++ JS_12_filter_obj /index.js
@@ -0,0 +1,26 @@
+const tasksArray = [{text:"first task",active:true},{text:"secund task",active:true},
+{text:"third task",active:false},{text:"forth task",active:true}];
+const ul = document.getElementById("ul");
+const bntAct = document.getElementById("bntAct");
+const bntAll = document.getElementById("bntAll");
+
+bntAct.onclick = () =>{
+   ul.innerHTML = "";
+   for (const task of tasksArray){
+      if(task.active){
+         const li =  document.createElement("li")
+         li.innerHTML = task.text
+         ul.appendChild(li)
+      }
+   }
+}
+bntAll.onclick = () =>{
+   ul.innerHTML = "";
+   for (const task of tasksArray){
+      
+         const li =  document.createElement("li")
+         li.innerHTML = task.text
+         ul.appendChild(li)
+      
+   }
+}
